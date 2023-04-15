d8b08ad5e41f0489eb70af9b8a139f588a7c7285
Alan Medina <alanmedinaalan@hotmail.com>
2023-04-14 23:04:04 -0300

    Inicio Mechs
    Creación del folder y updateo en Git

---
+++ Proyecto TDJ/Horror Game/Assets/Script/lightSwitch.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightSwitch : MonoBehaviour
+{
+    /*public GameObject inttext, light;
+    public bool toggle, interactable;
+    public Renderer lightBulb;
+    public Material offlight, onlight;
+    public AudioSource lightSwitchSound;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            inttext.SetActive(true);
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            inttext.SetActive(false);
+            interactable = false;
+        }    
+    }
+
+    void Update()
+    {
+        if(interactable == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                toggle = !toggle;
+                //lightswitchSound.Play();
+                switchAnim.ResetTrigger("press");
+                switchAnim.SetTrigger("press");
+            }
+        }
+        if(toggle == false)
+        {
+            light.SetActive(false);
+            lightBulb.material = offlight;
+        }
+
+        if (toggle == true)
+        {
+            light.SetActive(true);
+            lightBulb.material = onlight;
+        }
+    }*/
+}
