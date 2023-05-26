d75cd41669b57f1dba80d4c0a5c16ad5195f11da
Alan Medina <alanmedinaalan@hotmail.com>
2023-05-25 23:21:17 -0300

    Cambios de Texturas / ETC
    Menú de Escape, Resumen, implementación de texturas más HD.

--- Proyecto TDJ/Horror Game/Assets/Script/pause.cs
+++ Proyecto TDJ/Horror Game/Assets/Script/pause.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject pausemenu;
+    public string sceneName;
+    public bool toggle;
+    public SC_FPSController playerScript;
+
+    void FixedUpdate()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            toggle = !toggle;
+                if(toggle == false)
+            {
+                pausemenu.SetActive(false);
+                AudioListener.pause = false;
+                Time.timeScale = 1;
+                playerScript.enabled = true;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+                if(toggle == true)
+            {
+                pausemenu.SetActive(true);
+                AudioListener.pause = true;
+                Time.timeScale = 0;
+                playerScript.enabled = false;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            
+
+        }
+    }
+    public void resumeGame()
+    {
+        toggle = false;
+        pausemenu.SetActive(false);
+        AudioListener.pause = false;
+        Time.timeScale = 1;
+        playerScript.enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+   public void quitToMenu()
     {
-        
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(sceneName);
     }
 }
