2c43af0ee2c8dfe1d8db816854b1c54f60bf3f61
Sofía Godoy <soffxgdoy@hotmail.com>
2023-09-13 18:05:56 -0300

    13-9
    se saco el orden de recoleccion / se agregó un placeholder de logros

--- Proyecto TDJ/Horror Game/Assets/pickupcosas.cs
+++ Proyecto TDJ/Horror Game/Assets/pickupcosas.cs
@@ -6,7 +6,7 @@ public class pickupcosas : MonoBehaviour
 {
     // Start is called before the first frame update
     //EL LOCKED TEXT ES LO QUE SE USARIA PARA CUANDO EL OBJETO EST� BLOQUEADO
-    public GameObject PickUp, lockedtext1, lockedtext2, lockedtext3;
+    public GameObject PickUp, lockedtext1, lockedtext2, lockedtext3, logro_reg;
     public AudioSource RosesCollect;
     public AudioSource CandlesCollect;
     public AudioSource LighterCollect;
@@ -16,6 +16,8 @@ public class pickupcosas : MonoBehaviour
 
     public int CollectSteps = 0;
     public PlayerInventory playerInventory;
+
+    public bool recogido= false;
     void Start()
     {
         PickUp.SetActive(false);
@@ -39,45 +41,35 @@ public class pickupcosas : MonoBehaviour
                 if (this.gameObject.tag == "Rosas") {
                     
                     
-                    if (CollectSteps == 0){
+                    
                        
-                       this.gameObject.SetActive(false);
-                       playerInventory.RosesCollected(); 
+                    this.gameObject.SetActive(false);
+                    playerInventory.RosesCollected(); 
                        
                        
-                       RosesCollect.Play(); //sonideishon
+                    RosesCollect.Play(); //sonideishon
                        
                         
-                    }
+                    
                 }
 
                 if (this.gameObject.tag == "Velas") {
 
-                    /*if (CollectSteps != 1)
-                    {
-                        lockedText.SetActive(true);
-                        StopCoroutine("disableText");
-                        StartCoroutine("disableText");
-                    }*/
-
-                    if (CollectSteps == 1) {
+                   
                     
-                     this.gameObject.SetActive(false);
+                    
+                    this.gameObject.SetActive(false);
                      
-                     playerInventory.CandlesCollected();
+                    playerInventory.CandlesCollected();
                      
                    
-                     CandlesCollect.Play();
+                    CandlesCollect.Play();
 
-                    } else if (CollectSteps == 0) {
-                        lockedtext1.SetActive(true);
-                        StopCoroutine("disableText");
-                        StartCoroutine("disableText");
-                    }
+                   
                 }
 
                 if (this.gameObject.tag == "Encendedor") {
-                    if (CollectSteps== 2) {
+                   
                       this.gameObject.SetActive(false);
                       playerInventory.LighterCollected();
                       trigger_cuadro.SetActive(true);
@@ -86,29 +78,27 @@ public class pickupcosas : MonoBehaviour
                      LighterCollect.Play();
 
 
-                    } else if (CollectSteps!= 1) {
-                        lockedtext2.SetActive(true);
+                   
+                        /*lockedtext2.SetActive(true);
                         StopCoroutine("disableText");
-                        StartCoroutine("disableText");
-                    }
+                        StartCoroutine("disableText");*/ //locked text q me dejo de ejemplo
+                    
                 }
 
                 //turnOnsound.Play();
                 if (this.gameObject.tag == "Regalo")
                 {
-                    if (CollectSteps == 3)
-                    {
-                        this.gameObject.SetActive(false);
-                        playerInventory.GiftsCollected();
+                    
+                    
+                    this.gameObject.SetActive(false);
+                    playerInventory.GiftsCollected();
 
 
-                        GiftsCollect.Play();
+                    GiftsCollect.Play();
 
-                    } else if (CollectSteps != 3) {
-                        lockedtext3.SetActive(true);
-                        StopCoroutine("disableText");
-                        StartCoroutine("disableText");
-                    }
+                    
+                   
+                    
                 }
                 //ESTO SER�A EL CODIGO DONDE SE ACCIONARIA LO DE EL OBJETO BLOQUEADO
                 /*if (key.active == true)
@@ -128,7 +118,9 @@ public class pickupcosas : MonoBehaviour
     }
 
     //ESTO HACE QUE SE VEA EL TEXTO Y SE ACTIVE O DESACTIVE EN BASE A SI EL OBJETO EST� BLOQUEADO
-    IEnumerator disableText()
+ 
+    //corrutina q ahora no es necesaria pero q puede servir
+    /*IEnumerator disableText()
     {
         yield return new WaitForSeconds(1.0f);
         if (lockedtext1.activeInHierarchy == true) {
@@ -141,6 +133,14 @@ public class pickupcosas : MonoBehaviour
             lockedtext3.SetActive(false);
         }
         
+    }*/
+
+    IEnumerator disableLogro () {
+        yield return new WaitForSeconds(4.0f);
+        
+        if (logro_reg.activeInHierarchy == true) {
+            logro_reg.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -148,17 +148,34 @@ public class pickupcosas : MonoBehaviour
     {
        if (playerInventory.NumberOfRoses == 6) 
         {
-            CollectSteps=1;
+            //CollectSteps=1;
         }
 
        if(playerInventory.NumberOfCandles == 2) 
        {
-            CollectSteps=2;
+           
+           //CollectSteps=2;
        }
 
        if (playerInventory.NumberOfLighter == 1)
        {
-           CollectSteps = 3;
+           //CollectSteps = 3;
+       }
+
+       if (playerInventory.NumberOfGifts== 3){
+        //logro
+
+        if (recogido == false){
+            logro_reg.SetActive(true);
+            StopCoroutine("disableLogro");
+            StartCoroutine("disableLogro");
+
+            recogido = true;
+
+        }
+
+        
+
        }
 
     }
