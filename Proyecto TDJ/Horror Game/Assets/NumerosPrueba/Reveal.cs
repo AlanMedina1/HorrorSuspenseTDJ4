33e240f3e49bc5c35e3467640f3930054a57c0ad
Juan Fessia <juanchitofessia@gmail.com>
2024-11-27 07:49:04 -0300

    V_Numeros
    Agrege los numeros invisibles.

---
+++ Proyecto TDJ/Horror Game/Assets/NumerosPrueba/Reveal.cs
@@ -0,0 +1,18 @@
+﻿//Shady
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Reveal : MonoBehaviour
+{
+    [SerializeField] Material Mat;
+    [SerializeField] Light luzlint;
+	
+	void Update ()
+    {
+        if(Mat && luzlint){ 
+        Mat.SetVector("MyLightPosition",  luzlint.transform.position);
+        Mat.SetVector("MyLightDirection", -luzlint.transform.forward );
+        Mat.SetFloat ("MyLightAngle", luzlint.spotAngle         );
+        }
+    }//Update() end
+}//class end
